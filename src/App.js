7955e0fee71778d484c76a5e9374855f350767c6
mefa <mfbyrktr@gmail.com>
2020-02-29 02:15:51 +0300

    hadi

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 
 /* import logo from './logo.svg'; */
 import './App.css';
-import Navigation from "./components/Navigation/Navigation.js"
+/* import Navigation from "./components/Navigation/Navigation.js" */
 import Logo from "./components/Logo/Logo.js"
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js"
 import Rank from "./components/Rank/Rank.js"
