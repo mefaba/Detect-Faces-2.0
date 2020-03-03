93e40b3c3c662249b13e87ab5da256c12a986a44
mefa <mfbyrktr@gmail.com>
2020-03-03 21:45:10 +0300

    make it better

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -14,7 +14,7 @@ function Signin({onRouteChange,loadUser}) {
     }
 
     function onSubmitSignIn(){
-        fetch("http://localhost:3000/signin",{
+        fetch("https://warm-stream-92103.herokuapp.com/signin",{
             method: "post",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
